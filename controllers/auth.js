ea9d636fd8843ac9ffd4bd724886b7445200a913
MCarvalho <mcarvalho@the-8agency.com>
2021-06-10 14:35:09 -0300

    Reset Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
@@ -98,13 +99,39 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
         await user.save({ validateBeforeSave: false });
 
-        return next(new ErrorResponse('Email could not be sent', 500));
+        return next(new ErrorResponse('Email could not be sent', 500));  
     }
 
     res.status(200).json({
         success: true,
         data: user
     })
+})
+
+// @desc        Reset Password
+// @route       PUT /api/v1/auth/resetpassword/:resettoken
+// @access      Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+    // Get hashed token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resettoken).digest('hex');
+
+    
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    });
+
+    if(!user) {
+        return next(new ErrorResponse('Invalid token', 400));
+    }
+
+    // Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
 })
 
 // Get token from model, create cookie and send response
