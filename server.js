94929f69e77ca7552ade4c18179c9bb990cad8cc
MCarvalho <mcarvalho@the-8agency.com>
2021-06-11 15:48:57 -0300

    Prevent NoSQL Injection Sanitize Data

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');  
+const mongoSanitize = require('express-mongo-sanitize');  
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 // const logger = require('./middleware/logger');
@@ -39,6 +40,9 @@ if(process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// Sinitize data     
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
