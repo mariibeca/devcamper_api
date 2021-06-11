e51710e99d52d761a37df5a4fcc86483301bf9a3
MCarvalho <mcarvalho@the-8agency.com>
2021-06-11 12:45:54 -0300

    Add Review for Bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {
     getReviews,
-    getReview
+    getReview,
+    addReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -15,7 +16,8 @@ router.route('/')
     .get(advancedResults(Review, {
         path: 'bootcamp',
         select: 'name description'
-    }), getReviews);
+    }), getReviews)
+    .post(protect, authorize('user', 'admin'), addReview)
 
 router.route('/:id')
     .get(getReview)
