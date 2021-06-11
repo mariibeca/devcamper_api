d84d0f8d147e4a4802bdcb3e979691c5fa579052
MCarvalho <mcarvalho@the-8agency.com>
2021-06-11 17:08:48 -0300

    XSS Protection Security Headers

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');  
 const mongoSanitize = require('express-mongo-sanitize');  
+const helmet = require('helmet');  
+const xss = require('xss-clean');  
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 // const logger = require('./middleware/logger');
@@ -43,6 +45,12 @@ app.use(fileupload());
 
 // Sinitize data     
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
