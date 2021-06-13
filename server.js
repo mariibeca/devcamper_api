3507b374d2fd20d5922e4012bd64f254e0301a4f
MCarvalho <mcarvalho@the-8agency.com>
2021-06-13 17:34:32 -0300

    Rate Limiting, HPP CORS

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');  
 const helmet = require('helmet');  
 const xss = require('xss-clean');  
+const rateLimit = require('express-rate-limit');  
+const hpp = require('hpp');  
+const cors = require('cors');  
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 // const logger = require('./middleware/logger');
@@ -51,6 +54,19 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,  // 10 mins
+    max: 100
+});
+app.use(limiter);
+
+// Prevent http param polution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
